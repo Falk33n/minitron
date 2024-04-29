6841a593ade7f1de1bd8414e22210436b5643e76
Emil <emilj03@gmail.com>
2024-04-29 16:58:15 +0200

    Initialiszed the sidebar

--- minitronweb/src/app/layout.tsx
+++ minitronweb/src/app/layout.tsx
@@ -3,9 +3,16 @@ import type { Metadata } from 'next';
 import 'tailwindcss/tailwind.css';
 import { Logo } from '../components';
 import '../styles/globals.scss';
+import {
+	LucideFilePen,
+	LucideHome,
+	LucideNotebookPen,
+	LucidePen,
+	LucideUserCircle,
+} from 'lucide-react';
 
 export const metadata: Metadata = {
-	title: 'Minitron',
+	title: 'MinitronAI | Digital assistent',
 	description: 'Minitron AI',
 };
 
@@ -16,11 +23,19 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
-			<body className='h-screen bg-gradientGray'>
-				<nav>
+			<body className='bg-gradientBlue w-full h-screen flex justify-between items-center'>
+				<nav className='bg-white w-60 h-screen'>
 					<ul>
-            <li></li>
-          </ul>
+						<li>
+							Home <LucideHome />
+						</li>
+						<li>
+							Log In <LucideUserCircle />
+						</li>
+						<li>
+							New Chat <LucideFilePen />
+						</li>
+					</ul>
 				</nav>
 				{children}
 			</body>
