db66da67a58a6d46523de441efaa8a3261401b72
Max Planstedt <max.planstedt@gmail.com>
2024-05-21 10:08:34 +0200

    Changed to npm instead of yarn

--- minitronweb/src/helpers/index.ts
+++ minitronweb/src/helpers/index.ts
@@ -2,4 +2,4 @@ export * from './AI';
 export * from './adminData';
 export * from './accounts';
 
-export const baseURL = 'http://192.168.90.99:8080/api';
+export const baseURL = 'http://192.168.90.99:8081/api';
